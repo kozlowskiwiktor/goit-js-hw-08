3c1b1e21f7a0e61e0d0405be30259a9ff4f1e991
Wiktor Kozłowski <kozlowskiwiktor92@gmail.com>
2023-03-06 07:46:58 +0100

    taks 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,29 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector('.gallery')
+const markup = galleryItems
+     .map(image => 
+        `<a class="gallery__item" href="${image.original}">
+            <img class="gallery__image" 
+                src="${image.preview}"
+                alt="${image.description}"/>
+         </a>
+        `)
+    .join("");
+
+console.log(markup)
+
+list.insertAdjacentHTML('afterbegin', markup);
+
+// Opisany w dokumentacji
+import SimpleLightbox from "simplelightbox";
+// Dodatkowy import stylów
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionsDelay: 250,
+});
