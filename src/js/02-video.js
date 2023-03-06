1e11a306f2ae58809b68655b7b221820089fb534
Wiktor Kozłowski <kozlowskiwiktor92@gmail.com>
2023-03-06 20:46:38 +0100

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+     const onPlay = function(data) {
+        console.log(data);
+        localStorage.setItem('videoplayer-current-time', data.seconds);
+     };
+    
+     player.on('timeupdate', throttle(onPlay, 1000));
+
+    const currentTime = Number(localStorage.getItem('videoplayer-current-time')) || 0;
+
+    player.setCurrentTime(currentTime);
